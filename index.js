f22f82ccd6074e5e2d773d0ece6ad974ef2481ca
jdjj43 <jjaedej@gmail.com>
2024-02-16 11:08:18 -0400

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,9 +19,16 @@ app.get("/", function (req, res) {
 });
 
 
+
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", (req, res) => {
+  let isnum = /^\d+$/.test(req.params.date);
+  const date = new Date(isnum ? parseInt(req.params.date) : req.params.date);
+  res.json({
+    unix: Math.floor(date.getTime()), 
+     utc: date.toUTCString()
+  });
 });
 
 
